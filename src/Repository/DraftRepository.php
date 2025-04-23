a90ab02663323c945f871df034e5fe0089b1dfbf
github-ci <you@example.com>
2025-04-23 20:49:24 +0000

    chore: 更新代码

--- src/Repository/DraftRepository.php
+++ src/Repository/DraftRepository.php
@@ -4,7 +4,6 @@ namespace WechatOfficialAccountDraftBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use DoctrineEnhanceBundle\Repository\CommonRepositoryAware;
 use WechatOfficialAccountDraftBundle\Entity\Draft;
 
 /**
@@ -15,8 +14,6 @@ use WechatOfficialAccountDraftBundle\Entity\Draft;
  */
 class DraftRepository extends ServiceEntityRepository
 {
-    use CommonRepositoryAware;
-
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Draft::class);
