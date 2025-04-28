bc646a41f71bdf628684d8cd5a55ce00dcb6b114
github-ci <you@example.com>
2025-04-28 09:47:47 +0000

    fix: 修复字段错误

--- src/Entity/Draft.php
+++ src/Entity/Draft.php
@@ -30,7 +30,7 @@ class Draft
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
